b0b568c96fe60a42ab1b88145665848925e06d02
Elviz CR <zelvizcr@gmail.com>
2026-05-12 21:00:21 -0500

    exe

--- lib/core/constants/api_constants.dart
+++ lib/core/constants/api_constants.dart
@@ -1,17 +1,25 @@
+import 'dart:io';
+
 class ApiConstants {
   ApiConstants._();
 
-  static const String baseUrl = 'http://10.0.2.2:8080/api';
+  static String get baseUrl {
+    if (Platform.isAndroid) {
+      return 'http://10.0.2.2:8080/api';
+    } else {
+      return 'http://localhost:8080/api';
+    }
+  }
 
-  static const String authGoogle   = '/auth/google';
-  static const String register     = '/auth/register';
-  static const String login        = '/auth/login';
-  static const String recipes      = '/recipes';
-  static const String diseases     = '/users/diseases';
-  static const String recommended  = '/recipes/recommended';
-  static const String userProfile  = '/users/me';
+  static const String authGoogle = '/auth/google';
+  static const String register = '/auth/register';
+  static const String login = '/auth/login';
+  static const String recipes = '/recipes';
+  static const String diseases = '/users/diseases';
+  static const String recommended = '/recipes/recommended';
+  static const String userProfile = '/users/me';
   static const String userDiseases = '/users/me/diseases';
-  static const String chatbot      = '/chatbot/message';
-  static const String onboarding    = '/users/onboarding';
+  static const String chatbot = '/chatbot/message';
+  static const String onboarding = '/users/onboarding';
   static const String userProfileById = '/users';
 }
